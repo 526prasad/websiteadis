da4e6fe32390d15b50dfcddcc137edd84050f764
janakigouda <janakikangalgouda@gmail.com>
2021-04-10 15:18:49 +0530

    version 0.2

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import sujit from '../assets/sujit.jpg';
-import prasad from '../assets/prasad.jpg';
+import prasad from '../assets/prasadD.jpg';
 import md from '../assets/md.jpg';
 import { TitleComponent } from './TitleComponent';
 import team from '../assets/team.jpg';
@@ -23,62 +23,74 @@ function Team() {
                         </div>
                     </div>
                     <div className="team-body">
-                        <div className="card card-1">
-                            <div className="card-img">
-                                <img src={sujit} alt="sujit"></img>
-                            </div>
-                            <div className="desc">
-                                <div className="f-name">Sujit Hukkerikar</div>
-                                <div className="founder">Director</div>
-                                <div className="social-media">
-                                    <div>
-                                        <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+                        <div className="card">
+                            <div className="card-1">
+                                <div className="card-img">
+                                    <img src={sujit} alt="sujit"></img>
+                                </div>
+                                <div className="desc">
+                                    <div className="desc1">
+                                        <div className="f-name">Sujit Hukkerikar</div>
+                                        <div className="founder">Director</div>
                                     </div>
-                                    <div>
-                                        <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-twitter" aria-hidden="true"></i></a>
-                                    </div>
-                                    <div>
-                                        <a href="https://www.linkedin.com/in/sujithukkerikar/" target="_blank" rel="noopener noreferrer"> <i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                                    <div className="social-media">
+                                        <div>
+                                            <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+                                        </div>
+                                        <div>
+                                            <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-twitter" aria-hidden="true"></i></a>
+                                        </div>
+                                        <div>
+                                            <a href="https://www.linkedin.com/in/sujithukkerikar/" target="_blank" rel="noopener noreferrer"> <i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="card card-2">
-                            <div className="card-img">
-                                <img src={prasad} alt="prasad"></img>
-                            </div>
-                            <div className="desc">
-                                <div className="f-name">Prasad Desai</div>
-                                <div className="founder">Director</div>
-                                <div className="social-media">
-                                    <div>
-                                        <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+                        <div className="card">
+                            <div className="card-1">
+                                <div className="card-img">
+                                    <img src={prasad} alt="prasad"></img>
+                                </div>
+                                <div className="desc">
+                                    <div className="desc1">
+                                        <div className="f-name">Prasad Desai</div>
+                                        <div className="founder">Director</div>
                                     </div>
-                                    <div>
-                                        <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-twitter" aria-hidden="true"></i></a>
-                                    </div>
-                                    <div>
-                                        <a href="https://www.linkedin.com/in/prasad-desai-a532734b/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                                    <div className="social-media">
+                                        <div>
+                                            <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+                                        </div>
+                                        <div>
+                                            <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-twitter" aria-hidden="true"></i></a>
+                                        </div>
+                                        <div>
+                                            <a href="https://www.linkedin.com/in/prasad-desai-a532734b/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="card card-1">
-                            <div className="card-img">
-                                <img src={md} alt="md"></img>
-                            </div>
-                            <div className="desc">
-                                <div className="f-name">Madhav Hebbar</div>
-                                <div className="founder">Associate Director</div>
-                                <div className="social-media">
-                                    <div>
-                                        <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+                        <div className="card">
+                            <div className="card-1">
+                                <div className="card-img">
+                                    <img src={md} alt="md"></img>
+                                </div>
+                                <div className="desc">
+                                    <div className="desc1">
+                                        <div className="f-name">Madhav Hebbar</div>
+                                        <div className="founder">Associate Director</div>
                                     </div>
-                                    <div>
-                                        <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-twitter" aria-hidden="true"></i></a>
-                                    </div>
-                                    <div>
-                                        <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                                    <div className="social-media">
+                                        <div>
+                                            <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+                                        </div>
+                                        <div>
+                                            <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-twitter" aria-hidden="true"></i></a>
+                                        </div>
+                                        <div>
+                                            <a href="/" rel="noopener noreferrer" target="_blank"> <i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
--- src/components/carousel.js
+++ src/components/carousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import owl1 from '../assets/ddd.png';
 import owl2 from '../assets/certificate.jpg';
 import owl3 from '../assets/pr.jpg';
-import owl4 from '../assets/team.jpg';
+// import owl4 from '../assets/team.jpg';
 //Owl Carousel Libraries and Module
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
@@ -34,24 +34,24 @@ const option = {
 export default function Carousel() {
     return (
         <div className="about-down-container">
-            <div className="ab-head-name">Gallery</div>
+            <div className="ab-head-name">Achievements</div>
             <OwlCarousel className="slider-items owl-carousel" {...option}>
-            <div className="item">
+                <div className="item">
                     <div className="img certi"><img src={owl2} alt="owl2"/></div>
-                    <div className="i-name"><p>Our Achievement</p></div>
+                    <div className="i-name certi"><p>Winner Of Animal Husbandry StartUp Grand Challenge - 2021</p></div>
                 </div>
                 <div className="item">
                     <div className="img"><img src={owl1} alt="owl1"/></div>
-                    <div className="i-name"><p>Our Achievement</p></div>
+                    <div className="i-name"><p>Winner Of BIG BIRAC - 2021</p></div>
                 </div>
                 <div className="item">
                     <div className="img"><img src={owl3} alt="owl2"/></div>
-                    <div className="i-name"><p>Our Achievement</p></div>
+                    <div className="i-name"><p>Our Association With Rashtriya Kamdhenu Aayog</p></div>
                 </div>
-                <div className="item">
+                {/* <div className="item">
                     <div className="img"><img src={owl4} alt="owl3"/></div>
                     <div className="i-name"><p>Our Team</p></div>
-                </div>
+                </div> */}
             </OwlCarousel>
         </div>
     );
